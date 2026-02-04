eb71a8bdb7eb3c9baacec8b10ac33e3976e9d4b6
lexxdaniloff <55055797+lexdanilov@users.noreply.github.com>
2026-02-04 12:29:08 +0300

    upload

---
+++ data.js
@@ -0,0 +1,407 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0----",
+      "name": "Вид с птичьего полёта",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6441761588989152,
+          "pitch": 0.030388242684763256,
+          "rotation": 0,
+          "target": "1----"
+        },
+        {
+          "yaw": -0.7466593441889309,
+          "pitch": 0.12925767689431922,
+          "rotation": 0,
+          "target": "4----"
+        },
+        {
+          "yaw": 0.295240963138248,
+          "pitch": 0.13315911675822178,
+          "rotation": 0,
+          "target": "3-----"
+        },
+        {
+          "yaw": 0.3849795245809098,
+          "pitch": 0.05751299496720641,
+          "rotation": 0,
+          "target": "2----"
+        },
+        {
+          "yaw": 0.15362631486945766,
+          "pitch": 0.09410004315239107,
+          "rotation": 0,
+          "target": "5-----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1----",
+      "name": "Вид с птичьего полёта",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6869336775029389,
+          "pitch": -0.11718429479951098,
+          "rotation": 0,
+          "target": "0----"
+        },
+        {
+          "yaw": -1.3200227180508257,
+          "pitch": 0.2461781835780421,
+          "rotation": 0,
+          "target": "3-----"
+        },
+        {
+          "yaw": -0.8272323557945356,
+          "pitch": 0.2309986767447061,
+          "rotation": 0,
+          "target": "5-----"
+        },
+        {
+          "yaw": -1.1356734650743832,
+          "pitch": 0.07702042486159755,
+          "rotation": 0,
+          "target": "4----"
+        },
+        {
+          "yaw": 0.686884176783936,
+          "pitch": 0.25272919071034927,
+          "rotation": 0,
+          "target": "2----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2----",
+      "name": "Вид на детскую площадку",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.892745581924455,
+          "pitch": -0.22913478752443162,
+          "rotation": 0,
+          "target": "1----"
+        },
+        {
+          "yaw": -0.15543670006825572,
+          "pitch": -0.29439905531749666,
+          "rotation": 0,
+          "target": "0----"
+        },
+        {
+          "yaw": 0.3205016905957567,
+          "pitch": -0.005280000641924687,
+          "rotation": 0,
+          "target": "5-----"
+        },
+        {
+          "yaw": 0.4759994522627675,
+          "pitch": -0.0362465619019261,
+          "rotation": 0,
+          "target": "4----"
+        },
+        {
+          "yaw": -0.12544023095883006,
+          "pitch": -0.04321006978063657,
+          "rotation": 0,
+          "target": "3-----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-----",
+      "name": "Вид на водопад и амфитеатр",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6512502606351482,
+          "pitch": -0.009586439100452537,
+          "rotation": 0,
+          "target": "5-----"
+        },
+        {
+          "yaw": 0.011128832266717126,
+          "pitch": -0.035217441735058586,
+          "rotation": 0,
+          "target": "2----"
+        },
+        {
+          "yaw": 1.0749059663314569,
+          "pitch": -0.4580322926572542,
+          "rotation": 0,
+          "target": "1----"
+        },
+        {
+          "yaw": 2.6901882365707825,
+          "pitch": -0.3948063342192931,
+          "rotation": 0,
+          "target": "0----"
+        },
+        {
+          "yaw": -1.9045495299969453,
+          "pitch": -0.0734246652526025,
+          "rotation": 0,
+          "target": "4----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4----",
+      "name": "Вид на зону отдыха",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2227309388216625,
+          "pitch": -0.0315907621510636,
+          "rotation": 0,
+          "target": "5-----"
+        },
+        {
+          "yaw": 1.6978794930774663,
+          "pitch": -0.3218657534779332,
+          "rotation": 0,
+          "target": "0----"
+        },
+        {
+          "yaw": 0.594524228511979,
+          "pitch": -0.02057481689473306,
+          "rotation": 0,
+          "target": "3-----"
+        },
+        {
+          "yaw": 0.3801399650363777,
+          "pitch": -0.030041241621525216,
+          "rotation": 0,
+          "target": "2----"
+        },
+        {
+          "yaw": 0.8555433935603709,
+          "pitch": -0.22341420239179222,
+          "rotation": 0,
+          "target": "5-----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-----",
+      "name": "Вид на водопад и стеллу",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6968353512746344,
+          "pitch": -0.009158345871625784,
+          "rotation": 0,
+          "target": "2----"
+        },
+        {
+          "yaw": 2.854932857583819,
+          "pitch": 0.0022752510386236935,
+          "rotation": 0,
+          "target": "3-----"
+        },
+        {
+          "yaw": 1.733373485914984,
+          "pitch": -0.33757408794743426,
+          "rotation": 0,
+          "target": "1----"
+        },
+        {
+          "yaw": -3.097657190390189,
+          "pitch": -0.24942564628702257,
+          "rotation": 0,
+          "target": "0----"
+        },
+        {
+          "yaw": -1.9653718180651865,
+          "pitch": -0.030569247806136346,
+          "rotation": 0,
+          "target": "4----"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
